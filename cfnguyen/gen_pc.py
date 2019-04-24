e67efac1e7b98c5a0f28975df1faa4ab2e48dcf1
unknown <nguyen@DESKTOP-UDACG62.acb.acbcorp.vn>
2019-04-24 13:43:04 +0700

    add sql

--- cfnguyen/gen_pc.py
+++ cfnguyen/gen_pc.py
@@ -12,6 +12,37 @@ select
   '222'||code
 from
   pc_template
+  
+
+-- Table: public.c222
+
+-- DROP TABLE public.c222;
+
+CREATE TABLE public.c222
+(
+    code character(8) COLLATE pg_catalog."default" NOT NULL,
+    bill_id character varying(120) COLLATE pg_catalog."default",
+    price character varying(50) COLLATE pg_catalog."default",
+    callback_url character varying(240) COLLATE pg_catalog."default",
+    status integer DEFAULT 0,
+    CONSTRAINT code PRIMARY KEY (code)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.c222
+    OWNER to postgres;
+
+-- Index: c222idex
+
+-- DROP INDEX public.c222idex;
+
+CREATE INDEX c222idex
+    ON public.c222 USING btree
+    (code COLLATE pg_catalog."default")
+    TABLESPACE pg_default;  
 """
 l1 =  ['2', '3', '4', '5', '6', '7', '8', '9'] + list(string.ascii_lowercase)
 l2 =  ['2', '3', '4', '5', '6', '7', '8', '9'] + list(string.ascii_lowercase)
---
+++ cfnguyen/sql/1.sql
@@ -0,0 +1,23 @@
+﻿CREATE or REPLACE FUNCTION alloc_pc(tbl char(4),  _bill_id VARCHAR(120), _price VARCHAR(50), _callback_url VARCHAR(240)) RETURNS json  AS $$
+DECLARE 
+  t_row c222%ROWTYPE;
+  c VARCHAR(8);  
+BEGIN
+    EXECUTE format('select code from %s TABLESAMPLE SYSTEM(0.001)  where status=0 limit 1 for UPDATE SKIP LOCKED ;', tbl)
+    INTO c;
+    -- select * from c223 where status=0 ORDER BY random()  limit 1 for UPDATE  SKIP LOCKED  ;
+    -- select code into c  from quote_ident(tbl)  where  random() < 0.01 and  status=0  limit 1 for UPDATE SKIP LOCKED ;
+    IF c is NULL 
+    THEN
+        return NULL;
+    ELSE	
+        EXECUTE format('update %s set status = 1, bill_id=''%s'', price=''%s'', callback_url=''%s'' where code=''%s''',tbl, _bill_id, _price, _callback_url, c);
+	
+        -- select code into c  from c222 limit 1;
+        EXECUTE format('SELECT * FROM %s where code=''%s''', tbl, c)
+        INTO t_row ;
+        -- SELECT * into t_row FROM c222 where code=c;
+        RETURN row_to_json(t_row);
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
